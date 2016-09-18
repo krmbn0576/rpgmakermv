fc7b92c2dc4c6e876aa75553d8a574559bbc9470
unknown <krmbn0576@gmail.com>
2016-09-18 21:52:39 +0900

    IE非対応メッセージを追加

--- js/plugins/DataCacheBlocker.js
+++ js/plugins/DataCacheBlocker.js
@@ -18,6 +18,12 @@
  * なお、ブラウザ版以外で公開予定の場合はキャッシュに関する心配はまったく無いので、
  * その場合はこのプラグインを導入する必要はありません。
  * 
+ * おまけ：
+ * 「IEでは遊べません」とゲームの外に注意書きしても気づかない人がいるようなので、
+ * IEで遊び始めると「IEでは遊べません」とエラーを表示する機能を追加しました。
+ * 元々のWebAudio非対応云々なエラーメッセージよりは伝わりやすいはず。
+ * 
+ * ライセンス：
  * このプラグインの利用法に制限はありません。お好きなようにどうぞ。
  */
 
@@ -25,5 +31,14 @@
 	var _DataManager_loadDataFile = DataManager.loadDataFile;
 	DataManager.loadDataFile = function(name, src) {
 		_DataManager_loadDataFile.call(this, name, src + '?' + Date.now());
+	};
+
+	SceneManager.initAudio = function() {
+		var noAudio = Utils.isOptionValid('noaudio');
+		if (!WebAudio.initialize(noAudio) && !noAudio) {
+			var ua = navigator.userAgent.toLowerCase();
+			if (ua.indexOf('msie') > -1 || ua.indexOf('trident/7') > -1) throw new Error('このゲームはInternet Explorerでは遊べません。他のブラウザで遊んでね！');
+			else throw new Error('Your browser does not support Web Audio API.');
+		}
 	};
 })();
