088233f4bfb20ac1719d5c85bcd5f52cb1e1bd53
unknown <krmbn0576@gmail.com>
2016-09-24 23:23:55 +0900

    デバッグモニターを追加

--- js/plugins.js
+++ js/plugins.js
@@ -10,9 +10,10 @@ var $plugins =
 {"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyAbDEqyhj4YAhsv0Z05UVgo8wqc-7-_7mo","authDomain":"mosimo-online.firebaseapp.com","databaseURL":"https://mosimo-online.firebaseio.com","avatarEvent":"1","syncSwitchStart":"11","syncSwitchEnd":"20","syncVariableStart":"11","syncVariableEnd":"20"}},
 {"name":"DataCacheBlocker","status":true,"description":"キャッシュを阻止します。（dataフォルダのみ）","parameters":{}},
 {"name":"SymbolReplacer","status":false,"description":"ファイル名の!と$を別の語に置き換えます。","parameters":{"method":"restore","exclamationmark":"Exc_","dollar":"Dol_"}},
-{"name":"MakeScreenCapture","status":true,"description":"画面キャプチャ管理プラグイン","parameters":{"PNGキャプチャキー":"F6","JPEGキャプチャキー":"F7","WEBPキャプチャキー":"F9","ファイル名":"image","出力場所":"/captures","保存形式":"png","連番桁数":"2","タイムスタンプ":"ON","署名":"","署名サイズ":"22","署名画像":"","実行間隔":"0","効果音":""}},
+{"name":"MakeScreenCapture","status":true,"description":"画面キャプチャ管理プラグイン","parameters":{"PNGキャプチャキー":"F6","JPEGキャプチャキー":"F7","WEBPキャプチャキー":"F10","ファイル名":"image","出力場所":"/captures","保存形式":"png","連番桁数":"2","タイムスタンプ":"ON","署名":"","署名サイズ":"22","署名画像":"","実行間隔":"0","効果音":"Flash1"}},
 {"name":"MessageWindowHidden","status":true,"description":"メッセージウィンドウ一時消去プラグイン","parameters":{"ボタン名称":"control"}},
 {"name":"AdjustPictureGraphical","status":true,"description":"ピクチャのグラフィカルな位置調整プラグイン。\nパラメータを変更したら「プロジェクトの保存」（Ctrl+S）","parameters":{"グリッドサイズ":"48","テストマップID":"-1"}},
 {"name":"TMAutoNewGame","status":true,"description":"起動時に自動ではじめからゲームを開始します。\r\nWeb用ミニゲームなど、タイトルが不要な場合に使えます。","parameters":{"autoNewGame":"1","allwaysOnTop":"0","autoDevTool":"0"}},
-{"name":"HyperLink","status":true,"description":"ゲーム中に外部ページへのリンクを貼ります。","parameters":{"description":"Webサイトへのリンク"}}
+{"name":"HyperLink","status":true,"description":"ゲーム中に外部ページへのリンクを貼ります。","parameters":{"description":"Webサイトへのリンク"}},
+{"name":"DebugMonitor","status":true,"description":"スイッチ・変数の状態を画面上にリアルタイム表示します。","parameters":{"monitorX":"0","monitorY":"100","monitorW":"240"}}
 ];
---
+++ js/plugins/DebugMonitor.js
@@ -0,0 +1,200 @@
+//=============================================================================
+// DebugMonitor.js
+// PUBLIC DOMAIN
+//=============================================================================
+
+/*:
+ * @plugindesc スイッチ・変数の状態を画面上にリアルタイム表示します。
+ * @author くらむぼん
+ *
+ * @param monitorX
+ * @desc モニターのX座標
+ * @default 0
+ *
+ * @param monitorY
+ * @desc モニターのY座標
+ * @default 0
+ *
+ * @param monitorW
+ * @desc モニターの幅。「変数名が長くて潰れるんだけど！」という方は大きくしましょう
+ * @default 240
+ *
+ * @help
+ * デバッグメニューで指定したスイッチ・変数をモニターします。
+ * テストプレイ中のみ機能します。
+ * 
+ * 使い方：
+ * １．テストプレイ中にF9キーを押してデバッグメニューを表示
+ * ２．監視したいスイッチ・変数の上でShiftキーを押す
+ * 　　（監視対象に設定すると右端に*マークが表示されます）
+ * ３．デバッグメニューを閉じると指定座標にモニターが現れ、監視対象が表示されます
+ * 　　（通常のマップ上か、バトル中のみ表示されます。メニューや店などは非対応）
+ * ４．Ctrlキーを押すと、監視対象の値をリアルタイムに変更できるモードになります
+ * 　　（操作方法は普通のデバッグメニューと同じです。再度Ctrlで解除）
+ * 
+ * ライセンス：
+ * このプラグインの利用法に制限はありません。お好きなようにどうぞ。
+ */
+
+(function() {
+	'use strict';
+	if (!Utils.isOptionValid('test') || !Utils.isNwjs()) return;
+	var parameters = PluginManager.parameters('DebugMonitor');
+	var monitorX = +parameters['monitorX'] || 0;
+	var monitorY = +parameters['monitorY'] || 0;
+	var monitorW = +parameters['monitorW'] || 240;
+	var active = false;
+	var switches = {};
+	var variables = {};
+
+	function Window_Monitor() {
+		this.initialize.apply(this, arguments);
+	}
+
+	Window_Monitor.prototype = Object.create(Window_Command.prototype);
+	Window_Monitor.prototype.constructor = Window_Monitor;
+
+	Window_Monitor.prototype.initialize = function(x, y) {
+		Window_Command.prototype.initialize.call(this, x, y);
+		this.active = active;
+	};
+
+	Window_Monitor.prototype.windowWidth = function() {
+		return monitorW;
+	};
+
+	Window_Monitor.prototype.makeCommandList = function() {
+		for (var switchId in switches) {
+			if (switches[switchId]) {
+				this.addCommand('', 'S' + switchId);
+			}
+		}
+		for (var variableId in variables) {
+			if (variables[variableId]) {
+				this.addCommand('', 'V' + variableId);
+			}
+		}
+		if (!this._list.length) this.hide();
+	};
+
+	Window_Monitor.prototype.isOkEnabled = function() {
+		return false;
+	};
+
+	Window_Monitor.prototype.update = function() {
+		Window_Command.prototype.update.call(this);
+		if (Input.isRepeated('control')) active = this.active = !this.active;
+		for (var i = 0; i < this._list.length; i++) {
+			var k = this._list[i].symbol.charAt(0);
+			var n = this._list[i].symbol.charAt(1);
+			if (this.active && this.index() === i) {
+				if (k === 'S') {
+					if (Input.isRepeated('ok') || Input.isRepeated('left') || Input.isRepeated('right')) {
+						SoundManager.playCursor();
+						$gameSwitches.setValue(n, !$gameSwitches.value(n));
+					}
+				} else {
+					var value = $gameVariables.value(n);
+					if (Input.isRepeated('right')) {
+						value++;
+					}
+					if (Input.isRepeated('left')) {
+						value--;
+					}
+					if (Input.isRepeated('pagedown')) {
+						value += 10;
+					}
+					if (Input.isRepeated('pageup')) {
+						value -= 10;
+					}
+					if ($gameVariables.value(n) !== value) {
+						$gameVariables.setValue(n, value);
+						SoundManager.playCursor();
+					}
+				}
+			}
+			if (k === 'S') {
+				var name = $dataSystem.switches[n];
+				var newValue = (name ? name : 'S[' + n.padZero(4) + ']') + ':' + ($gameSwitches.value(n) ? '[ON]' : '[OFF]');
+			} else {
+				var name = $dataSystem.variables[n];
+				var newValue = (name ? name : 'V[' + n.padZero(4) + ']') + ':' + $gameVariables.value(n);
+			}
+			if (this._list[i].name !== newValue) {
+				this._list[i].name = newValue;
+				var update = true;
+			}
+		}
+		if (update) {
+			this.contents.clear();
+			this.drawAllItems();
+		}
+	};
+
+	Window_Monitor.prototype.isOpenAndActive = function() {
+		return this.isOpen() && this.active;
+	};
+
+
+
+	var _Window_DebugEdit_update = Window_DebugEdit.prototype.update;
+	Window_DebugEdit.prototype.update = function() {
+		_Window_DebugEdit_update.apply(this, arguments);
+		this.updateMonitor();
+	};
+
+	Window_DebugEdit.prototype.updateMonitor = function() {
+		if (this.active && Input.isRepeated('shift')) {
+			if (this._mode === 'switch') {
+				var switchId = this.currentId();
+				switches[switchId] = !switches[switchId];
+			} else {
+				var variableId = this.currentId();
+				variables[variableId] = !variables[variableId];
+			}
+			SoundManager.playCursor();
+			this.redrawCurrentItem();
+		}
+	};
+
+	Window_DebugEdit.prototype.itemStatus = function(dataId) {
+		if (this._mode === 'switch') {
+			return ($gameSwitches.value(dataId) ? '[ON]' : '[OFF]') + (switches[dataId] ? '*' : '');
+		} else {
+			return String($gameVariables.value(dataId)) + (variables[dataId] ? '*' : '');
+		}
+	};
+
+
+
+	var _Window_Selectable_isOpenAndActive = Window_Selectable.prototype.isOpenAndActive;
+	Window_Selectable.prototype.isOpenAndActive = function() {
+		return !active && _Window_Selectable_isOpenAndActive.apply(this, arguments);
+	};
+
+	var _Game_Player_canMove = Game_Player.prototype.canMove;
+	Game_Player.prototype.canMove = function() {
+		return !active && _Game_Player_canMove.apply(this, arguments);
+	};
+
+
+
+	var _Scene_Map_createAllWindows = Scene_Map.prototype.createAllWindows;
+	Scene_Map.prototype.createAllWindows = function() {
+		_Scene_Map_createAllWindows.apply(this, arguments);
+		this.createMonitorWindow();
+	};
+
+	var _Scene_Battle_createAllWindows = Scene_Battle.prototype.createAllWindows;
+	Scene_Battle.prototype.createAllWindows = function() {
+		_Scene_Battle_createAllWindows.apply(this, arguments);
+		this.createMonitorWindow();
+	};
+
+	function createMonitorWindow() {
+		this._monitorWindow = new Window_Monitor(monitorX, monitorY);
+		this.addWindow(this._monitorWindow);
+	}
+
+	Scene_Map.prototype.createMonitorWindow = Scene_Battle.prototype.createMonitorWindow = createMonitorWindow;
+})();
